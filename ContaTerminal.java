f417a329179d53627477cda40f1ee4e0f9b7be26
eduardoPetrocchi <eduardopetrocchi@hotmail.com>
2023-08-29 20:40:35 -0300

    Exercício - ContaBancaria

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,7 +1,23 @@
 package ContaBanco.ContaBanco;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("taum");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite o número da sua conta: ");
+                int conta = scanner.nextInt();
+            System.out.println("Digite o número da sua agencia: ");
+                String agencia = scanner.next();
+            System.out.println("Digite o seu nome: ");
+                String nome = scanner.next();
+            System.out.println("Digite o seu saldo: ");
+                double saldo = scanner.nextFloat();
+
+        scanner.close();
+
+        System.out.println("Olá" + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ",com a conta corrento número " + conta + ".Seu saldo é de R$"+saldo+", e já encontra-se disponível para saque!");
     }
 }
